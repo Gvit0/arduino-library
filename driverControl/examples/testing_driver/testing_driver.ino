3a3e8de2f47be09f5865f76c31cd0993dabdece1
Gvit0 <90312305+Gvit0@users.noreply.github.com>
2022-10-20 07:25:26 +0300

    Add files via upload

--- driverControl/examples/testing_driver/testing_driver.ino
+++ driverControl/examples/testing_driver/testing_driver.ino
@@ -11,19 +11,19 @@ ENB-10
 driverControl drive1(2, 3, 4, 5, 9, 10);
 
 void setup() {
-  drive1.forward(255, 255);
+  drive1.forward(255);
   delay(1000);
-  drive1.forward(128, 128);
+  drive1.forward(128);
   delay(1000);
-  drive1.back(255, 255);
+  drive1.back(255);
   delay(1000);  
-  drive1.left(255, 255);
+  drive1.left(255);
   delay(1000); 
-  drive1.right(255, 255);
+  drive1.right(255);
   delay(1000);   
-  drive1.tright(255, 255);
+  drive1.tright(255);
   delay(1000); 
-  drive1.tleft(255, 255);
+  drive1.tleft(255);
   delay(1000); 
   drive1.stop();
 }
