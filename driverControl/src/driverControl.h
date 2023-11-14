df68828dcf029b728f12c115704052e064df99b2
Gvit0 <90312305+Gvit0@users.noreply.github.com>
2023-11-14 17:12:13 +0300

    Update driverControl.h
    edit class driverControl

--- driverControl/src/driverControl.h
+++ driverControl/src/driverControl.h
@@ -2,7 +2,7 @@
 class driverControl {
   private:
     int _sPR;
-	int _sPL;
+    int _sPL;
     int _IN1;
     int _IN2;
     int _IN3;
@@ -13,13 +13,13 @@ class driverControl {
     int _jX;
     int _jY;
   public:
-    driverControl(int IN1, int IN2, int IN3, int IN4, int PinSpeedA, int PinSpeedB);
+    driverControl(int IN1, int IN2, int IN3, int IN4, int ENA, int ENB);
     void forward(int _Speed);
     void back(int _Speed);
     void left(int _Speed);
     void right(int _Speed);
-	  void stop();
-	  void tleft(int _Speed);
+    void stop();
+    void tleft(int _Speed);
     void tright(int _Speed);
     void joystick(int _jX,int _jY);
 };
